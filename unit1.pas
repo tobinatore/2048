b9af88f481f0a0d34e095a0300a363e47485ff10
Tobias <germanicalp@gmail.com>
2017-01-13 20:20:53 +0100

    Commit #3 -Fixes
    - fixed tile merging and moving
    - added color change on merge
    - updated lose condition to work correctly
    - 0's are no longer shown on empty fields
    - started working on color schemes other than the original

--- unit1.pas
+++ unit1.pas
@@ -22,11 +22,10 @@ type
     MainMenu1: TMainMenu;
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
-    MenuItem3: TMenuItem;
-    MenuItem4: TMenuItem;
     procedure btn_resetClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure MenuItem2Click(Sender: TObject);
   private
     { private declarations }
   public
@@ -40,11 +39,13 @@ type
   end;
 
  type a_container = array[1..6,1..6] of r_tile;
+ type a_colors = array[1..9000] of TColor;
 
  var
   Form1: TForm1;
   tile_null : r_tile;
   field: a_container;
+  colors: a_colors;
   points : Integer;
 
 implementation
@@ -52,6 +53,8 @@ implementation
 {$R *.lfm}
 
 { TForm1 }
+
+//DRAW_FIELD_____________________________
 procedure drawField();
 var x,y,mx,my : Integer;
 begin
@@ -62,6 +65,8 @@ begin
    Form1.Image1.Canvas.Rectangle(x*90,y*90,(x+1)*90,(y+1)*90);
    mx := (x*90+(x+1)*90) Div 2 - 3;
    my := (y*90+(y+1)*90) Div 2 - 5;
+
+   if field[y+1,x+1].value <> 0 then
    Form1.Image1.Canvas.TextOut(mx,my,inttostr(field[y+1,x+1].value));
   end;
  end;
@@ -71,16 +76,41 @@ begin
 end;
 
 //____TEST_OB_FELD_VOLL___||___TESTING_FIELD_FOR_FREE_SPACES____
-function isFull():Boolean;
+function isFull(simpleCheck:Char):Boolean;
 var noCellsFree : Boolean;
     y,x : Integer;
 begin
 
  noCellsFree := true;
 
+ if simpleCheck = 'n' then begin
  for y := 1 to 6 do begin
     for x := 1 to 6 do begin
-     if field[y,x].value = tile_null.value then noCellsFree := false;
+     if field[y,x].value = tile_null.value then begin
+        noCellsFree := false;
+     end
+     else if field[y,x].value=field[y+1,x].value then begin
+        noCellsFree := false;
+     end
+     else if field[y,x].value=field[y-1,x].value then begin
+        noCellsFree := false;
+     end
+     else if field[y,x].value=field[y,x+1].value then begin
+        noCellsFree := false;
+     end
+     else if field[y,x].value=field[y,x-1].value then begin
+        noCellsFree := false;
+     end;
+    end;
+  end;
+  end
+ else begin
+  for y := 1 to 6 do begin
+    for x := 1 to 6 do begin
+     if field[y,x].value = tile_null.value then begin
+        noCellsFree := false;
+     end;
+ end;
     end;
   end;
 
@@ -93,12 +123,15 @@ procedure generateNewTile(rounds : Integer);
 var x,y,i : Integer;
 begin
 
- if isFull() then begin
+ if isFull('n') then begin
    showmessage('Verloren');
  end
  else begin
  for i := 1 to rounds do begin
 
+  if isFull('y') then begin
+    end
+  else begin
     repeat
       x := random(6)+1;
       y := random(6)+1;
@@ -109,6 +142,7 @@ begin
     field[y,x].color := RGBToColor(230,230,230);
 
     drawField();
+  end;
  end;
 end;
 end;
@@ -142,18 +176,15 @@ begin
 
 for x:=1 to 6 do begin
 for y:=1 to 5 do begin
-if (field[y,x].value) = (field[y+1,x].value) then begin
-   field[y,x].value:= field[y,x].value+ field[y+1,x].value;
-   field[y+1,x].value:=0; //erst mit Verschiebung sinnvoll
-end;
 i := y+1;
 while ((field[i-1,x].value) = 0) and (i > 1) do begin
    field[i-1,x].value:=field[i,x].value;
    field[i-1,x].color:=field[i,x].color;
    field[i,x].value:= 0;
-   field[i,x].color:= RGBToColor(255,255,255);//erst mit Verschiebung sinnvoll
+   field[i,x].color:= RGBToColor(255,255,255);
    i -= 1;
 end;
+
 end;
 end;
 
@@ -161,7 +192,21 @@ for x:=1 to 6 do begin
 for y:=1 to 5 do begin
 if (field[y,x].value) = (field[y+1,x].value) then begin
    field[y,x].value:= field[y,x].value+ field[y+1,x].value;
-   field[y+1,x].value:=0; //erst mit Verschiebung sinnvoll
+   field[y+1,x].value:=0;
+   field[y,x].color := colors[field[y,x].value];
+end;
+end;
+end;
+
+for x:=1 to 6 do begin
+for y:=1 to 5 do begin
+i := y+1;
+while ((field[i-1,x].value) = 0) and (i > 1) do begin
+   field[i-1,x].value:=field[i,x].value;
+   field[i-1,x].color:=field[i,x].color;
+   field[i,x].value:= 0;
+   field[i,x].color:= RGBToColor(255,255,255);
+   i -= 1;
 end;
 end;
 end;
@@ -174,23 +219,15 @@ procedure onDown();
 var x,y,i: integer;
 begin
 
-for x:=1 to 6 do begin
-for y:=6 downto 2 do begin
-if (field[y,x].value) = (field[y-1,x].value) then begin
-   field[y,x].value:= field[y,x].value+ field[y-1,x].value;
-   field[y-1,x].value:=0; //erst mit Verschiebung sinnvoll
-end;
-end;
-end;
 
 for x:=1 to 6 do begin
-for y:=1 to 5 do begin
+for y:=6 downto 1 do begin
 i := y+1;
 while ((field[i,x].value) = 0) and (i < 7) do begin
    field[i,x].value:=field[i-1,x].value;
    field[i,x].color:=field[i-1,x].color;
    field[i-1,x].value:= 0;
-   field[i-1,x].color:= RGBToColor(255,255,255);//erst mit Verschiebung sinnvoll
+   field[i-1,x].color:= RGBToColor(255,255,255);
    i += 1;
 end;
 end;
@@ -200,7 +237,21 @@ for x:=1 to 6 do begin
 for y:=6 downto 2 do begin
 if (field[y,x].value) = (field[y-1,x].value) then begin
    field[y,x].value:= field[y,x].value+ field[y-1,x].value;
-   field[y-1,x].value:=0; //erst mit Verschiebung sinnvoll
+   field[y-1,x].value:=0;
+   field[y,x].color := colors[field[y,x].value];
+end;
+end;
+end;
+
+for x:=1 to 6 do begin
+for y:=6 downto 1 do begin
+i := y+1;
+while ((field[i,x].value) = 0) and (i < 7) do begin
+   field[i,x].value:=field[i-1,x].value;
+   field[i,x].color:=field[i-1,x].color;
+   field[i-1,x].value:= 0;
+   field[i-1,x].color:= RGBToColor(255,255,255);
+   i += 1;
 end;
 end;
 end;
@@ -214,10 +265,24 @@ var y,x,i: integer;
 begin
 
  for y:=1 to 6 do begin
- for x:=6 downto 2 do begin
+for x:=5 downto 1 do begin
+ i := x+1;
+ while ((field[y,i].value) = 0) and (i < 7) do begin
+    field[y,i].value:=field[y,i-1].value;
+    field[y,i].color:=field[y,i-1].color;
+    field[y,i-1].value:= 0;
+    field[y,i-1].color:= RGBToColor(255,255,255);
+    inc(i);
+ end;
+ end;
+ end;
+
+ for y:=1 to 6 do begin
+ for x:=5 downto 1 do begin
  if (field[y,x].value) = (field[y,x+1].value) then begin
     field[y,x+1].value:=field[y,x].value+ field[y,x+1].value;
-    field[y,x].value:=0; //erst mit Verschiebung sinnvoll
+    field[y,x].value:=0;
+    field[y,x+1].color := colors[field[y,x+1].value];
  end;
  end;
  end;
@@ -229,17 +294,8 @@ for x:=5 downto 1 do begin
     field[y,i].value:=field[y,i-1].value;
     field[y,i].color:=field[y,i-1].color;
     field[y,i-1].value:= 0;
-    field[y,i-1].color:= RGBToColor(255,255,255);//erst mit Verschiebung sinnvoll
+    field[y,i-1].color:= RGBToColor(255,255,255);
     inc(i);
- end;
- end;
- end;
-
- for y:=1 to 6 do begin
- for x:=6 downto 2 do begin
- if (field[y,x].value) = (field[y,x+1].value) then begin
-    field[y,x+1].value:=field[y,x].value+ field[y,x+1].value;
-    field[y,x].value:=0; //erst mit Verschiebung sinnvoll
  end;
  end;
  end;
@@ -252,11 +308,26 @@ procedure onLeft();
 var y,x,i: integer;
 begin
 
+
+for y:=1 to 6 do begin
+for x:=2 to 6 do begin
+ i := x-1;
+ while ((field[y,i].value) = 0) and (i > 0) do begin
+    field[y,i].value:=field[y,i+1].value;
+    field[y,i].color:=field[y,i+1].color;
+    field[y,i+1].value:= 0;
+    field[y,i+1].color:= RGBToColor(255,255,255);
+    i -= 1;
+ end;
+ end;
+ end;
+
 for y:=1 to 6 do begin
  for x:=1 to 5 do begin
  if (field[y,x].value) = (field[y,x+1].value) then begin
     field[y,x].value:=field[y,x].value+ field[y,x+1].value;
-    field[y,x+1].value:=0; //erst mit Verschiebung sinnvoll
+    field[y,x+1].value:=0;
+    field[y,x].color := colors[field[y,x].value];
  end;
  end;
  end;
@@ -268,17 +339,8 @@ for x:=2 to 6 do begin
     field[y,i].value:=field[y,i+1].value;
     field[y,i].color:=field[y,i+1].color;
     field[y,i+1].value:= 0;
-    field[y,i+1].color:= RGBToColor(255,255,255);//erst mit Verschiebung sinnvoll
+    field[y,i+1].color:= RGBToColor(255,255,255);
     i -= 1;
- end;
- end;
- end;
-
-for y:=1 to 6 do begin
- for x:=1 to 5 do begin
- if (field[y,x].value) = (field[y,x+1].value) then begin
-    field[y,x].value:=field[y,x].value+ field[y,x+1].value;
-    field[y,x+1].value:=0; //erst mit Verschiebung sinnvoll
  end;
  end;
  end;
@@ -287,6 +349,7 @@ for y:=1 to 6 do begin
 
 //________INITIALISIERUNG___||___INITALIZATION________
 procedure TForm1.FormCreate(Sender: TObject);
+var i: Integer;
 begin
 
   Image1.Canvas.Brush.Color := clWhite;
@@ -295,6 +358,23 @@ begin
   Image1.Canvas.Clear;
   Image1.Canvas.Clear; //Durch einen Bug in Lazarus wird es bei nur einem Mal ausführen schwarz gefärbt
 
+  for i:= 1 to 9000 do begin
+
+  case i of
+  2: colors[i]:=RGBToColor(238,228,218);
+  4: colors[i]:=RGBToColor(237,224,200);
+  8: colors[i]:=RGBToColor(242,177,121);
+  16: colors[i]:=RGBToColor(245,149,99);
+  32: colors[i]:=RGBToColor(246,124,95);
+  64: colors[i]:=RGBToColor(246,94,59);
+  128: colors[i]:=RGBToColor(237,207,114);
+  256: colors[i]:=RGBToColor(237,204,97);
+  512: colors[i]:=RGBToColor(237,200,80);
+  1024: colors[i]:=RGBToColor(237,197,63);
+  2048: colors[i]:=RGBToColor(237,194,46);
+  else colors[i]:=RGBToColor(255,255,255);
+
+  end;
 
   tile_null.value := 0;
   tile_null.caption:= ' ';
@@ -303,7 +383,7 @@ begin
   reset();
 
 end;
-
+ end;
 //________BENUTZER_SETZT_SPIEL_ZURÜCK___||___USER_RESETS_GAME________
 procedure TForm1.btn_resetClick(Sender: TObject);
 begin
@@ -323,6 +403,15 @@ begin
  VK_Right: onRight();
  VK_Left: onLeft();
  end;
+
+end;
+
+
+
+procedure TForm1.MenuItem2Click(Sender: TObject);
+begin
+
+ //NIGHTMODE xD
 
 end;
 
